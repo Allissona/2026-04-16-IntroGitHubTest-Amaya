5efdb97f4ffa56a26ae59ee17c895710228f67ca
allisson amaya <aamaya4@sfsu.edu>
2026-04-16 16:28:53 -0700

    Initial Commit

---
+++ 2026-04-16-IntroGitHubTest-Amaya/_026_04_16_IntroGitHubTest_AmayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _026_04_16_IntroGitHubTest_AmayaApp.swift
+//  2026-04-16-IntroGitHubTest-Amaya
+//
+//  Created by patron on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _026_04_16_IntroGitHubTest_AmayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
